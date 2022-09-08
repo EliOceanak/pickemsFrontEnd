5a09f1540a293d6c1fe3b8519df5888feb36c409
eli <eli@pop-os.localdomain>
2022-09-08 17:10:25 -0500

    not much

--- src/app/odds.service.ts
+++ src/app/odds.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/core'
 
 @Injectable({
   providedIn: 'root'
 })
 export class OddsService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor() { }
 }
